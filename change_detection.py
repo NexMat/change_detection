ff9bff145841c920d80889a7d9124663b21e875e
NexMat <vu.mathieu@gmail.com>
2017-11-23 00:21:58 +0100

    writing function assign probabilities

--- change_detection.py
+++ change_detection.py
@@ -10,7 +10,7 @@ from estim_dist import estimate_parameter
 
 # TODO read_opt
 
-def assign_proba(img):
+def assign_proba(img, lambda_est):
 
     voisinage = parcourir_image(lr_img)
 
@@ -31,3 +31,100 @@ def assign_proba(img):
                         proba[i][j] += 0.05
 
     return proba
+
+def test():
+    
+    # parametres
+    img_size = 255
+    lambda_bruit = 20
+
+    # creation de deux images degrades
+    img1 = np.array([[i for i in range(img_size)] for j in range(img_size)])
+    img2 = np.array([[i for i in range(img_size)] for j in range(img_size)])
+
+    # modification de la deuxieme image:
+    # creation de deux lignes uniformes sur toute la longueur
+    img2[img_size // 2] = [10 for i in range(img_size)]
+    img2[(img_size // 2) + 1] = [10 for i in range(img_size)]
+
+    # bruitage des images
+    img1 = img1 + np.random.rayleigh(lambda_bruit, (img_size, img_size))
+    img2 = img2 + np.random.rayleigh(lambda_bruit, (img_size, img_size))
+
+    data = np.array([lgn[-40:] for lgn in img2[-40:]]) # echantillon de taille 40x40 en bas à droite
+
+    params = estimate_parameter(data, "rayleigh")
+    lambda_est = params[1]
+
+    assign_prob(img2, lambda_est)
+
+
+
+#    proba = assign_probar(lr_img)
+#
+#    q1, q2, q3 = 0, 0, 0
+#    # modification des valeurs pour l'affichage
+#    for i in range(img_size):
+#        for j in range(img_size):
+#            if lr_img[i][j] >= 2:
+#                lr_img[i][j] = 250
+#                q1 += 1
+#            elif lr_img[i][j] >= 1:
+#                lr_img[i][j] = 200
+#                q2 += 1
+#            elif lr_img[i][j] >= 0:
+#                q3 += 1
+#                lr_img[i][j] = 50
+#
+#    #print("Nombre de pixels changés:         ", img_size * 2)
+#    #print("Valeurs entre 0 inclus et 1 exclu:", q3)
+#    #print("Valeurs entre 1 inclus et 2 exclu:", q2)
+#    #print("Valeurs supérieures a 2 inclus:   ", q1)
+#
+#    valeurs = []
+#    for lgn in proba:
+#        #print(lgn)
+#        for val in lgn:
+#            if val != 0 and val not in valeurs:
+#                valeurs.append(val)
+#
+#    valeurs = np.sort(valeurs)
+#
+#    print("Probabilités assignées:")
+#    for val in valeurs:
+#        print(val)
+#
+#    print("Pour quitter: q")
+#    while True:
+#
+#        try:
+#            param_seuil = input("Entrez la valeur du seuil: ")
+#        except EOFError:
+#            print()
+#            break
+#
+#        if param_seuil == 'q':
+#            break
+#        else:
+#            param_seuil = float(param_seuil)
+#
+#        lr_img_smooth = np.zeros((img_size, img_size))
+#
+#        for i in range(img_size):
+#            for j in range(img_size):
+#                if proba[i][j] >= param_seuil: # parametre
+#                    lr_img_smooth[i][j] = 250
+#
+#        # affiche les images en niveaux de gris
+#        plt.subplot(141) # premiere image
+#        plt.imshow(img1, cmap = "gray", vmin = 1, vmax = 256)
+#        plt.subplot(142) # deuxieme image
+#        plt.imshow(img2, cmap = "gray", vmin = 1, vmax = 256)
+#        plt.subplot(143) # image des differences
+#        plt.imshow(lr_img, cmap = "gray", vmin = 1, vmax = 256)
+#        plt.subplot(144) # image des differences lissee
+#        plt.imshow(lr_img_smooth, cmap = "gray", vmin = 1, vmax = 256)
+#        plt.show()
+
+if __name__ == '__main__':
+    test()
