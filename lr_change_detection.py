357d3f929a391b15d58f8eb3963f17b334976a54
NexMat <vu.mathieu@gmail.com>
2017-12-07 16:32:32 +0100

    ajout de la courbe ROC

--- lr_change_detection.py
+++ lr_change_detection.py
@@ -42,8 +42,6 @@ def assign_proba_lr(lr_img):
         
 
 
-
-
 def test():
 
     # parametres
@@ -132,5 +130,100 @@ def test():
         plt.imshow(lr_img_smooth, cmap = "gray", vmin = 1, vmax = 256)
         plt.show()
 
+def test_param():
+
+    # parametres
+    img_size = 255
+    lambda_bruit = 20
+
+    # creation de deux images degrades
+    img1 = np.array([[i for i in range(img_size)] for j in range(img_size)])
+    img2 = np.array([[i for i in range(img_size)] for j in range(img_size)])
+
+    # modification de la deuxieme image:
+    # creation de deux lignes uniformes sur toute la longueur
+    img2[img_size // 2] = [10 for i in range(img_size)]
+    img2[(img_size // 2) + 1] = [10 for i in range(img_size)]
+
+    img_orig = np.copy(img2)
+
+    # bruitage des images
+    img1 = img1 + np.random.rayleigh(lambda_bruit, (img_size, img_size))
+    img2 = img2 + np.random.rayleigh(lambda_bruit, (img_size, img_size))
+
+    # log ratio
+    lr_img = log_ratio(img1, img2)
+
+    proba = assign_proba_lr(lr_img)
+
+    q1, q2, q3 = 0, 0, 0
+    # modification des valeurs pour l'affichage
+    for i in range(img_size):
+        for j in range(img_size):
+            if lr_img[i][j] >= 2:
+                lr_img[i][j] = 250
+                q1 += 1
+            elif lr_img[i][j] >= 1:
+                lr_img[i][j] = 200
+                q2 += 1
+            elif lr_img[i][j] >= 0:
+                q3 += 1
+                lr_img[i][j] = 50
+
+    #print("Nombre de pixels changés:         ", img_size * 2)
+    #print("Valeurs entre 0 inclus et 1 exclu:", q3)
+    #print("Valeurs entre 1 inclus et 2 exclu:", q2)
+    #print("Valeurs supérieures a 2 inclus:   ", q1)
+
+    valeurs = []
+    for lgn in proba:
+        #print(lgn)
+        for val in lgn:
+            if val != 0 and val not in valeurs:
+                valeurs.append(val)
+
+    valeurs = np.sort(valeurs)
+
+    positifs = []
+    fausse_alarme = []
+
+    for i in range(100):
+
+        param_seuil = i / 100
+
+        lr_img_smooth = np.zeros((img_size, img_size))
+
+        for i in range(img_size):
+            for j in range(img_size):
+                if proba[i][j] >= param_seuil: # parametre
+                    lr_img_smooth[i][j] = 250
+
+        count_positive, count_FA = count_detection(img_orig, lr_img_smooth)
+
+        positifs.append(count_positive)
+        fausse_alarme.append(count_FA)
+
+    plt.xlabel('Fausses alarmes')
+    plt.ylabel('Bonne détection')
+    plt.plot(fausse_alarme, positifs)
+    plt.show()
+
+def count_detection(img_orig, detection):
+    count_FA = 0
+    count_positive = 0
+
+    for i in range(len(img_orig)):
+        for j in range(len(img_orig[0])):
+            if i == (len(img_orig) // 2) or i == (len(img_orig) // 2) + 1:
+                if detection[i][j] == 250:
+                    count_positive += 1
+
+            elif detection[i][j] == 250:
+                count_FA += 1
+    
+    return count_positive, count_FA
+
+
+
 if __name__ == '__main__':
-    test()
+    test_param()
