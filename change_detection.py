806befba58e821d6b4ec05289ca2a93ade4b2644
NexMat <vu.mathieu@gmail.com>
2017-12-19 16:44:53 +0100

    detection de changement selon le modele terminee. Reste a optimiser les parametres

--- change_detection.py
+++ change_detection.py
@@ -12,8 +12,8 @@ from estim_dist import estimate_parameter
 # TODO read_opt
 
 threshold = 0.8
-p_i = 0.1 # prior
-p_v = 0.4 # observation
+p_i = 0.2 # prior
+p_v = 0.1 # observation
 
 def assign_proba(img, lambda_est):
 
@@ -30,15 +30,18 @@ def assign_proba(img, lambda_est):
     for i in range(len(img)):
         for j in range(len(img[0])):
 
+            index_vsn = index_voisins(i, j, haut_img, long_img)
+
             # p_v
-            for k,l in index_voisins(i, j, haut_img, long_img):
-                if (i, j) != (k, l):
+            for index in index_vsn:
+                if index != None and index != (i, j):
+                    k, l = index
                     if is_superior(img[k][l], lambda_est, threshold):
-                        proba[i][j] += p_v #TODO
+                        proba[i][j] += p_v 
 
 
             # p_i
-            if is_superior(img[i][j], lambda_est, threshold):
+            if is_superior(img[i][j], lambda_est, threshold): #TODO adjust probability
                 proba[i][j] += p_i
 
     return proba
@@ -69,24 +72,65 @@ def test():
 
     # creation de deux images a partir d'une distribution de rayleigh
     img1 = np.random.rayleigh(lambda_r, (img_size, img_size))
-    img2 = np.copy(img1)
+    img2 = np.random.rayleigh(lambda_r, (img_size, img_size))
+
+    for i in range(img_size):
+        for j in range(img_size):
+            if img1[i][j] > 255:
+                img1[i][j] = 255
+
+            if img2[i][j] > 255:
+                img2[i][j] = 255
 
     # modification de la deuxieme image:
     # creation de deux lignes uniformes sur toute la longueur
-    img2[img_size // 2] = [10 for i in range(img_size)]
-    img2[(img_size // 2) + 1] = [10 for i in range(img_size)]
+    img2[img_size // 2] = [250 for i in range(img_size)]
+    img2[(img_size // 2) + 1] = [250 for i in range(img_size)]
 
     echantillon = np.array([lgn[-40:] for lgn in img2[-40:]]) # echantillon de taille 40x40 en bas à droite
 
     params = estimate_parameter(echantillon, "rayleigh")
     lambda_est = params[1]
 
-    probs = assign_prob(img2, lambda_est)
+    proba = assign_proba(img2, lambda_est)
+
+    # valeurs des probabilités prises
+    val_probas = {}
+    for lgn in proba:
+        for val in lgn:
+            if val not in val_probas.keys():
+                val_probas[val] = 1
+            else:
+                val_probas[val] += 1
+
+    for key in sorted(val_probas.keys()): 
+        print(key, val_probas[key])
+
+    #for i in range(100):
+
+    #param_seuil = i / 100
+    param_seuil = 0.7
+
+    # initialisation de la detection finale
+    adv_detection = np.zeros((img_size, img_size))
+
+    for j in range(img_size):
+        for k in range(img_size):
+            if proba[j][k] >= param_seuil:
+                adv_detection[j][k] = 250
+
+    # affiche les images en niveaux de gris
+    plt.subplot(131) # premiere image
+    plt.imshow(img1, cmap = "gray", vmin = 1, vmax = 256)
+    plt.subplot(132) # deuxieme image
+    plt.imshow(img2, cmap = "gray", vmin = 1, vmax = 256)
+    plt.subplot(133) # image des differences
+    plt.imshow(adv_detection, cmap = "gray", vmin = 1, vmax = 256)
+    plt.show()
 
 
-
-
-if __name__ == '__main__':
+def plot_cumulative():
+    
     size = 1000
     param = 80
     percentage_treshold = 0.6
@@ -97,9 +141,13 @@ if __name__ == '__main__':
     cumulative = np.cumsum((values / size) * 100) # evaluate the cumulative (normalized)
 
     plt.subplot(121)
-    plt.hist(data) # plot the cumulative function
+    plt.hist(data) # plot the histogram
     plt.subplot(122)
-    plt.plot(cumulative) # plot the survival function
+    plt.plot(cumulative) # plot the cumulative function
     
     plt.show()
+
+
+if __name__ == '__main__':
+    test()
 
