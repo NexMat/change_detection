eb7752efc1c7218044af2425f0d500547185a0f4
NexMat <vu.mathieu@gmail.com>
2017-12-19 16:45:25 +0100

    test sur une image synthetique avec les valeurs distribuees selon une loi de rayleigh

--- lr_change_detection.py
+++ lr_change_detection.py
@@ -134,22 +134,34 @@ def test_param():
 
     # parametres
     img_size = 255
-    lambda_bruit = 20
+    lambda_bruit = 50
 
     # creation de deux images degrades
-    img1 = np.array([[i for i in range(img_size)] for j in range(img_size)])
-    img2 = np.array([[i for i in range(img_size)] for j in range(img_size)])
+    #img1 = np.array([[i for i in range(img_size)] for j in range(img_size)])
+    #img2 = np.array([[i for i in range(img_size)] for j in range(img_size)])
+
+    # creation de deux images a partir d'une distribution de rayleigh
+    img1 = np.random.rayleigh(lambda_bruit, (img_size, img_size))
+    img2 = np.random.rayleigh(lambda_bruit, (img_size, img_size))
+
+    for i in range(img_size):
+        for j in range(img_size):
+            if img1[i][j] > 255:
+                img1[i][j] = 255
+
+            if img2[i][j] > 255:
+                img2[i][j] = 255
 
     # modification de la deuxieme image:
     # creation de deux lignes uniformes sur toute la longueur
-    img2[img_size // 2] = [10 for i in range(img_size)]
-    img2[(img_size // 2) + 1] = [10 for i in range(img_size)]
+    img2[img_size // 2] = [250 for i in range(img_size)]
+    img2[(img_size // 2) + 1] = [250 for i in range(img_size)]
 
     img_orig = np.copy(img2)
 
     # bruitage des images
-    img1 = img1 + np.random.rayleigh(lambda_bruit, (img_size, img_size))
-    img2 = img2 + np.random.rayleigh(lambda_bruit, (img_size, img_size))
+    #img1 = img1 + np.random.rayleigh(lambda_bruit, (img_size, img_size))
+    #img2 = img2 + np.random.rayleigh(lambda_bruit, (img_size, img_size))
 
     # log ratio
     lr_img = log_ratio(img1, img2)
@@ -175,17 +187,21 @@ def test_param():
     #print("Valeurs entre 1 inclus et 2 exclu:", q2)
     #print("Valeurs supérieures a 2 inclus:   ", q1)
 
-    valeurs = []
-    for lgn in proba:
-        #print(lgn)
-        for val in lgn:
-            if val != 0 and val not in valeurs:
-                valeurs.append(val)
+    #valeurs = []
+    #for lgn in proba:
+    #    #print(lgn)
+    #    for val in lgn:
+    #        if val != 0 and val not in valeurs:
+    #            valeurs.append(val)
 
-    valeurs = np.sort(valeurs)
+    #valeurs = np.sort(valeurs)
 
     positifs = []
     fausse_alarme = []
+
+    seuil_optimal = 0
+    compromis = 0
+    lr_img_smooth_opti = None
 
     for i in range(100):
 
@@ -193,19 +209,44 @@ def test_param():
 
         lr_img_smooth = np.zeros((img_size, img_size))
 
-        for i in range(img_size):
-            for j in range(img_size):
-                if proba[i][j] >= param_seuil: # parametre
-                    lr_img_smooth[i][j] = 250
+        for j in range(img_size):
+            for k in range(img_size):
+                if proba[j][k] >= param_seuil: # parametre
+                    lr_img_smooth[j][k] = 250
 
         count_positive, count_FA = count_detection(img_orig, lr_img_smooth)
+
+        # Normalisation
+        count_positive /= (img_size * 2)
+        count_FA /= ((img_size**2) - (img_size * 2))
 
         positifs.append(count_positive)
         fausse_alarme.append(count_FA)
 
-    plt.xlabel('Fausses alarmes')
-    plt.ylabel('Bonne détection')
-    plt.plot(fausse_alarme, positifs)
+        compromis_tmp = count_positive + (1 - count_FA)
+
+        if compromis_tmp >= compromis:
+            compromis = compromis_tmp
+            seuil_optimal = param_seuil
+            lr_img_smooth_opti = lr_img_smooth.copy()
+
+
+    plt.xlabel('Fausses alarmes (%)')
+    plt.ylabel('Bonne détection (%)')
+    plt.plot(fausse_alarme, positifs, '+b')
+    plt.show()
+
+    print(seuil_optimal)
+
+    # affiche les images en niveaux de gris
+    plt.subplot(141) # premiere image
+    plt.imshow(img1, cmap = "gray", vmin = 1, vmax = 256)
+    plt.subplot(142) # deuxieme image
+    plt.imshow(img2, cmap = "gray", vmin = 1, vmax = 256)
+    plt.subplot(143) # image des differences
+    plt.imshow(lr_img, cmap = "gray", vmin = 1, vmax = 256)
+    plt.subplot(144) # image des differences lissee
+    plt.imshow(lr_img_smooth_opti, cmap = "gray", vmin = 1, vmax = 256)
     plt.show()
 
 def count_detection(img_orig, detection):
